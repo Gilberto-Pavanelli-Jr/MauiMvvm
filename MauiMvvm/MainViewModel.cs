409f7ff3487666bd7fd5ad616e48059c0e2583ff
Gilberto Pavanelli <gilberto.pavanelli.jr@gmail.com>
2022-05-10 22:28:25 -0300

    Add project files.

---
+++ MauiMvvm/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiMvvm
+{
+    //[INotifyPropertyChanged]
+    internal partial class MainViewModel : ObservableObject
+    {
+        public string CounterString
+        {
+            get
+            {
+                return $"Current count: {counter}";
+            }
+        }
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(CounterString))]
+        private int counter;
+
+        [ICommand]
+        public void IncrementCounter()
+        {
+            Counter++;
+        }
+    }
+}
